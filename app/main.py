b305283a8f245baad435bfde9cfda7ca43f4f443
vdjuketic <v.djuketic@gmail.com>
2023-12-02 19:47:18 +0100

    receive multiple messages

--- app/main.py
+++ app/main.py
@@ -7,11 +7,12 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, _ = server_socket.accept()  # wait for client
 
-    message = conn.recv(1024).decode()
-    print(f"received message: {message}")
+    while True:
+        message = conn.recv(1024).decode()
+        print(f"received message: {message}")
 
-    if message == "*1\r\n$4\r\nping\r\n":
-        conn.sendall("+PONG\r\n".encode())
+        if message == "*1\r\n$4\r\nping\r\n":
+            conn.sendall("+PONG\r\n".encode())
 
 
 if __name__ == "__main__":
